f46ff800bbc1371ebffda1b0f544f6c780f1068d
Matthew Aquino <matthewaquino96@gmail.com>
2020-12-28 21:28:23 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,11 +1,14 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
     10.times do
       string = "s" + string
-    p string  
+    end  
+    p string
+    binding.pry
   else
     string
   end
